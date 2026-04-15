199e38f58c261359138eef3188705333f5f5d853
L1F3atbhss <Nathan.chan3000@gmail.com>
2026-04-15 15:35:02 -0400

    Update puzzles.py

--- puzzles.py
+++ puzzles.py
@@ -1,3 +1,14 @@
 def puzzle1():
     print("You enter a puzzle room!")
+
+
+
+def puzzle2():
+    print("You enter a puzzle room!")
+    print("")
+
+
+
+def puzzle3():
+    print("You enter a puzzle room!")
     
